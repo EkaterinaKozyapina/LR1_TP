24a7a1baab7486b79b61b8a4a71b4185d2435bcb
EkaterinaKozyapina <ekaterina_kozypina@mail.ru>
2021-11-08 22:10:44 +0300

    Добавление последнего дочернего класса

--- ConsoleApplication1/Fantasist.cpp
+++ ConsoleApplication1/Fantasist.cpp
@@ -1,1 +1,33 @@
 #include "Fantasist.h"
+
+Fantasist::Fantasist() {
+	filmExists = false;
+
+	cout << "\t\tКласс Fantasist: конструктор без параметров" << endl;
+}
+
+Fantasist::Fantasist(string fio, int yearOfBird, int yearOfDeath, vector<string> works, bool filmExists) {
+	this->filmExists = filmExists;
+
+	cout << "\t\tКласс Fantasist: конструктор с параметрами" << endl;
+}
+
+Fantasist::Fantasist(const Fantasist & fantasist) {
+	fio = fantasist.fio;
+	yearOfBird = fantasist.yearOfBird;
+	yearOfDeath = fantasist.yearOfDeath;
+	works = fantasist.works;
+	filmExists = fantasist.filmExists;
+
+	cout << "\t\tКласс Fantasist: конструктор копирования" << endl;
+}
+
+Fantasist::~Fantasist() {
+	filmExists = false;
+
+	cout << "\t\tКласс Fantasist: деструктор" << endl;
+}
+
+void Fantasist::setFilmExists(bool filmExists) { this->filmExists = filmExists; }
+
+bool Fantasist::getFilmExists() { return filmExists; }
--- ConsoleApplication1/Fantasist.h
+++ ConsoleApplication1/Fantasist.h
@@ -4,5 +4,25 @@
 class Fantasist : public Publication
 {
 public:
+	// Конструктор без параметров
+	Fantasist();
+
+	// Конструктор с параметрами (ФИО, года рождения и смерти, некоротые работы, сняты ли фильмы)
+	Fantasist(string fio, int yearOfBird, int yearOfDeath, vector<string> works, bool filmExists);
+
+	// Конструктор копирования
+	Fantasist(const Fantasist & fantasist);
+
+	// Деструктор
+	~Fantasist();
+
+	// Установление краткой биографии
+	void setFilmExists(bool filmExists);
+
+	// Извлечение краткой биографии
+	bool getFilmExists();
+
 private:
+
+	bool filmExists;			// Сняты ли фильмы по книгам
 };
