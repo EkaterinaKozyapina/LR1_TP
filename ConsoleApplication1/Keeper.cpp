719a3c7563d2c5d4ff45b832af25db7ff2ba4824
EkaterinaKozyapina <ekaterina_kozypina@mail.ru>
2021-11-08 22:18:49 +0300

    Создание контейнера

---
+++ ConsoleApplication1/Keeper.cpp
@@ -0,0 +1,66 @@
+#include "Keeper.h"
+
+Keeper::Keeper() {
+	size = 0;
+	index = 0;
+	conteiner = nullptr;
+
+	cout << "\t\tКласс Keeper: конструктор без параметров" << endl;
+}
+
+Keeper::Keeper(int size) {
+	this->size = size;
+	conteiner = new Publication*[size];
+	index = 0;
+
+	cout << "\t\tКласс Keeper: конструктор с параметрами" << endl;
+}
+
+Keeper::Keeper(const Keeper & keeper) {
+	size = keeper.size;
+	for (int i = 0; i < keeper.index; i++)
+		addObject(*keeper.conteiner[i]);
+
+	cout << "\t\tКласс Keeper: конструктор копирования" << endl;
+}
+
+Keeper::~Keeper() {
+	int cnt = index;
+	for (int i = 0; i < cnt; i++)
+		removeObject();
+	size = 0;
+	index = 0;
+	conteiner = nullptr;
+
+	cout << "\t\tКласс Keeper: деструктор" << endl;
+}
+
+void Keeper::saveToFile() {
+
+}
+
+void Keeper::extractFromFile() {
+
+}
+
+std::ostream& operator<< (std::ostream &out, const Keeper & keeper)
+{
+
+	out << endl;
+
+	return out;
+}
+
+void Keeper::addObject(Publication & publication) {
+	if (index == size)
+		throw exception("Контейнер заполнен, добавление объекта невозможно");
+	conteiner[index] = &publication;
+	index++;
+}
+
+void Keeper::removeObject() {
+	if (index == 0)
+		throw exception("Контейнер пуст, удаление объекта невозможно");
+	delete(conteiner[index - 1]);
+	index--;
+}
---
+++ ConsoleApplication1/Keeper.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Publication.h"
+
+class Keeper
+{
+public:
+
+	// Конструктор без параметров
+	Keeper();
+
+	// Конструктор с параметрами (размер контейнера)
+	Keeper(int size);
+
+	// Конструктор копирования
+	Keeper(const Keeper & keeper);
+
+	// Деструктор
+	~Keeper();
+
+	// Сохранение контейнера в файл
+	void saveToFile();
+
+	// Извлечь контейнер из файла
+	void extractFromFile();
+
+	// Вывод контейнера на экран (перегрузка оператора вывода <<)
+	friend std::ostream& operator<< (std::ostream &out, const Keeper & keeper);
+
+	// Добавление элемента в конец контейнера
+	void addObject(Publication & publication);
+
+	// Удаление элемента с конца контейнера
+	void removeObject();
+
+private:
+
+	int size;						// Размер контейнера
+	int index;						// Количество элементов в контейнере
+	Publication ** conteiner;		// Контейнер (массив указателей на базовый класс)
+};
